89b5e4bfb146b119b2bdfea651fd29fdeadf7cc7
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-20 19:39:24 +0530

    Add files via upload

---
+++ LOKESHXMUSIC/__init__.py
@@ -0,0 +1,26 @@
+from LOKESHXMUSIC.core.bot import Anony
+from LOKESHXMUSIC.core.dir import dirr
+from LOKESHXMUSIC.core.git import git
+from LOKESHXMUSIC.core.userbot import Userbot
+from LOKESHXMUSIC.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Anony()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
---
+++ LOKESHXMUSIC/plugins/tools/ping.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from LOKESHXMUSIC import app
+from LOKESHXMUSIC.core.call import Anony
+from LOKESHXMUSIC.utils import bot_sys_stats
+from LOKESHXMUSIC.utils.decorators.language import language
+from LOKESHXMUSIC.utils.inline import supp_markup
+from config import BANNED_USERS, PING_IMG_URL
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await Anony.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
