d596839bfea9d3dd838544c26d256edb46390da9
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-21 17:24:39 +0530

    𝓛𝓞𝓚𝓔𝓢𝓗 𝓧 𝓜𝓤𝓢𝓘𝓒

--- LOKESHXMUSIC/platforms/Youtube.py
+++ LOKESHXMUSIC/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from LOKESHXMUSIC.utils.database import is_on_off
+from LOKESHXMUSIC.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
