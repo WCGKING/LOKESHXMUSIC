3586688b6abdd1d72f29c7764e9dc2c94a32aba9
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-21 18:37:49 +0530

    𝓛𝓞𝓚𝓔𝓢𝓗 𝓧 𝓜𝓤𝓢𝓘𝓒

--- LOKESHXMUSIC/plugins/misc/autoleave.py
+++ LOKESHXMUSIC/plugins/misc/autoleave.py
@@ -27,7 +27,7 @@ async def auto_leave():
                             if (
                                 i.chat.id != config.LOGGER_ID
                                 and i.chat.id != -1001626004802
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1001860470873
                             ):
                                 if left == 20:
                                     continue
