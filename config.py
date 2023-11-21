0342fec34145b835fd49bffbfaacb9e60dc14776
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-21 12:33:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO", 
-    "https://github.com/WCGKING/Lokesh_up82"
+    "https://github.com/WCGKING/LOKESHXMUSIC"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
