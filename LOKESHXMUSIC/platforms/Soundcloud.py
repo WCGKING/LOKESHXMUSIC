50cde70a891eb96f8eeb72c2163b5f87322ddd5f
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-21 17:22:23 +0530

    𝓛𝓞𝓚𝓔𝓢𝓗 𝓧 𝓜𝓤𝓢𝓘𝓒

--- LOKESHXMUSIC/platforms/Soundcloud.py
+++ LOKESHXMUSIC/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from AnonXMusic.utils.formatters import seconds_to_min
+from LOKESHXMUSIC.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
