1622e529d90de280cfb1e6d4f7a46d95b3ff76f6
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-21 17:56:14 +0530

    play.py

--- LOKESHXMUSIC/utils/inline/play.py
+++ LOKESHXMUSIC/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "II—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—II————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——II———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———II——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————II—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————II————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————II———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————II——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————II—"
     else:
-        bar = "—————————◉"
+        bar = "—————————II"
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
