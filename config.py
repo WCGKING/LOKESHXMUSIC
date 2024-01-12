fa207615cdd38a3bfa32ceccfbcd1c94af0b80b3
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-12 19:15:43 +0530

    𝓛𝓞𝓚𝓔𝓢𝓗 𝓧 𝓜𝓤𝓢𝓘𝓒

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1800))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/727c460f645e466742dde.jpg"
+    "START_IMG_URL", "https://graph.org/file/ff564784dc65b2e867b2d.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL","https://te.legra.ph/file/004189d06206d224048e6.jpg"
+    "PING_IMG_URL","https://graph.org/file/ec604b9b0030bb8806af4.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
