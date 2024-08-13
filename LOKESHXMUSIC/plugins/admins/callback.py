334f0ac3acdc46c5064cdd699637c5aaf7e259c8
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-08-13 17:28:37 +0530

    𝓛𝓞𝓚𝓔𝓢𝓗 𝓧 𝓜𝓤𝓢𝓘𝓒

--- LOKESHXMUSIC/plugins/admins/callback.py
+++ LOKESHXMUSIC/plugins/admins/callback.py
@@ -3,7 +3,7 @@ import asyncio
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from LOKESHXMUSIC import YouTube, app, YTB
+from LOKESHXMUSIC import YouTube, app
 from LOKESHXMUSIC.core.call import Anony
 from LOKESHXMUSIC.misc import SUDOERS, db
 from LOKESHXMUSIC.utils.database import (
@@ -252,15 +252,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                     video=status,
                 )
             except:
-                try:
-                    file_path, direct = await YTB.download(
-                        videoid,
-                        mystic,
-                        videoid=True,
-                        video=status,
-                    )
-                except:
-                    return await mystic.edit_text(_["call_6"])
+                return await mystic.edit_text(_["call_6"])
             try:
                 image = await YouTube.thumbnail(videoid, True)
             except:
