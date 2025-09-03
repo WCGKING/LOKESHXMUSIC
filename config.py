e26cd200a02f18bd145fe08d8bdc0370c07a5194
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-09-03 13:50:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1800))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002120932809))
+LOGGER_ID = int(getenv("LOGGER_ID", -1001754457302))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6258877205))
