94bd08e25f81af2d78e17dc13f3d1417b417591e
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-12-17 15:38:59 +0530

    𝓛𝓞𝓚𝓔𝓢𝓗 𝓧 𝓜𝓤𝓢𝓘𝓒

--- config.py
+++ config.py
@@ -30,6 +30,10 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz')
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots6c30dd')
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO", 
     "https://github.com/WCGKING/LOKESHXMUSIC"
