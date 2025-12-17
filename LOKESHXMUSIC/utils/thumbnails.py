27ba6abacb3f9caf235fb00c189f7aba5f311d6e
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-12-17 22:43:05 +0530

    𝓛𝓞𝓚𝓔𝓢𝓗 𝓧 𝓜𝓤𝓢𝓘𝓒

--- LOKESHXMUSIC/utils/thumbnails.py
+++ LOKESHXMUSIC/utils/thumbnails.py
@@ -122,7 +122,7 @@ async def get_thumb(videoid):
             font4 = ImageFont.truetype('LOKESHXMUSIC/assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "LOKESH MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((10, 10), "KHUSHI VIBES", fill="white", font = font1, align ="left") 
             image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
