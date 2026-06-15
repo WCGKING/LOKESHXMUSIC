9b0e8cdafc1b79460e2ac42bd99282b70ae4c89f
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2026-06-15 11:13:03 +0530

    Update YouTube.py

--- LOKESHXMUSIC/platforms/YouTube.py
+++ LOKESHXMUSIC/platforms/YouTube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from RadhaMusic.utils.database import is_on_off
-from RadhaMusic.utils.formatters import time_to_seconds
+from LOKESHXMUSIC.utils.database import is_on_off
+from LOKESHXMUSIC.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
@@ -26,7 +26,7 @@ async def shell_cmd(cmd):
     return out.decode("utf-8")
 
 
-cookies_file = "RadhaMusic/assets/cookies.txt"
+cookies_file = "cookies/LOKESHXMUSIC"
 
 class YouTubeAPI:
     def __init__(self):
